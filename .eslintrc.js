e437610d5eb2656ba48a7acb245223fd7eaf72d6
Rubén <rubenaguadoc@gmail.com>
2019-11-01 10:35:58 +0100

    Solver comma-dangle probelm

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,7 +41,16 @@ module.exports = {
     'object-property-newline': 'warn',
     'prefer-arrow-callback': 'error',
     'space-before-function-paren': 'off',
-    'comma-dangle': ['error', 'always-multiline'],
+    'comma-dangle': [
+      "error",
+      {
+        "arrays": "always-multiline",
+        "objects": "always-multiline",
+        "imports": "always-multiline",
+        "exports": "always-multiline",
+        "functions": "never"
+      }
+    ],
   },
   "plugins": ["prettier"]
 };
