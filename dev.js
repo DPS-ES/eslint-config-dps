3b196c9da2b9d13f23ab13790198ea93dc053203
Rubén <rubenaguadoc@gmail.com>
2022-08-28 14:39:26 +0200

    refactor: :recycle: Move Sonar rules to only dev config

--- dev.js
+++ dev.js
@@ -3,5 +3,7 @@ const prodConfig = require('./.eslintrc.json');
 prodConfig.rules['no-console'] = 'warn';
 prodConfig.rules['no-debugger'] = 'warn';
 prodConfig.rules['no-unused-vars'] = 'warn';
+prodConfig.rules['sonarjs/no-duplicate-string'] = 'warn';
+prodConfig.rules['sonarjs/cognitive-complexity'] = 'warn';
 
 module.exports = prodConfig;
