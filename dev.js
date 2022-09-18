8a214d943f373bcc6f5a041a96fd942b63546c4c
Rubén <rubenaguadoc@gmail.com>
2022-09-18 20:30:11 +0200

    refactor: :rotating_light: Remove warnings for sonarjs/cognitive-complexity & sonarjs/no-duplicate-string

--- dev.js
+++ dev.js
@@ -3,7 +3,5 @@ const prodConfig = require('./.eslintrc.json');
 prodConfig.rules['no-console'] = 'warn';
 prodConfig.rules['no-debugger'] = 'warn';
 prodConfig.rules['no-unused-vars'] = 'warn';
-prodConfig.rules['sonarjs/no-duplicate-string'] = 'warn';
-prodConfig.rules['sonarjs/cognitive-complexity'] = 'warn';
 
 module.exports = prodConfig;
